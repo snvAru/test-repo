d066e8db68bfb67d8a1ad6d64b2ea6bc337fc85d
Аружан <aru@MacBook-Air-Aruzan.local>
2022-10-18 21:13:54 +0600

    task with folders

---
+++ script.js
@@ -0,0 +1,40 @@
+let desktop = [
+    {   
+        name: "Folder 1",
+        folders: [
+            {name: "Subfolder 1"},
+            {name: "Subfolder 2"},
+            {name: "Subfolder 3"},
+        ]
+    },
+    {   
+        name: "Folder 2",
+        folders: [
+            {name: "Subfolder 4"},
+            {name: "Subfolder 5"},
+            {name: "Subfolder 6"},
+        ]
+    },
+    {   
+        name: "Folder 3",
+        folders: [
+            {name: "Subfolder 7"},
+            {name: "Subfolder 8"},
+            {name: "Subfolder 9"},
+        ]
+    }
+]
+
+function showParentFolderName(subfolderName){
+    for(let i=0; i< desktop.length; i++){
+        for(let k=0; k< desktop[i].folders.length; k++){
+            if(subfolderName === desktop[i].folders[k].name){
+                console.log(desktop[i].name)
+                break
+            }
+        }
+    }
+}
+
+
+showParentFolderName("Subfolder 5")
